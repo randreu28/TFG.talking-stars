3d0bd8720b07a9988c19c3850ae1910682552bfa
randreu28 <randreu28@gmail.com>
2022-10-23 13:59:53 +0200

    Initalize using vite

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex bg-gray-900 text-white">
+      <div className="m-auto text-4xl">Hello world!</div>
+    </div>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import glsl from "vite-plugin-glsl";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [glsl(), react()],
+});
