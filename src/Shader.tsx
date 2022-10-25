1da5659033b2d176f5a7b61359dd688075f197ce
randreu28 <randreu28@gmail.com>
2022-10-25 11:35:27 +0200

    Gets average hz of media stream

--- src/Shader.tsx
+++ src/Shader.tsx
@@ -1,14 +1,26 @@
 import { Box } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 interface Props {
   stream: MediaStream;
 }
 
-//ref:
-//https://github.com/mrdoob/three.js/blob/master/examples/webaudio_visualizer.html
-
 export default function Shader({ stream }: Props) {
-  //..handleStream and display shader
+  //Creates an analyser for the media stream
+  const audioCtx = new AudioContext();
+  const mic = audioCtx.createMediaStreamSource(stream);
+  const analyser = audioCtx.createAnalyser();
+  const FFTData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.fftSize = analyser.fftSize / Math.pow(2, 3); // default is 2048
+  mic.connect(analyser);
+
+  //Logs the average Hz each render frame
+  useFrame(() => {
+    analyser.getByteFrequencyData(FFTData);
+    const avg = FFTData.reduce((prev, cur) => prev + cur / FFTData.length, 0);
+    console.log(avg);
+  });
+
   return (
     <>
       <Box />
